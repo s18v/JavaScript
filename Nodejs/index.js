a819207d1dfb310d9d2df04b5e7a01dc26a51c32
Saran Vellanki <sarankumarvellanki@gmail.com>
2015-05-21 12:56:55 -0700

    Add callbacks

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,15 +1,79 @@
 var fs = require('fs')
 var express = require('express')
 var app = express();
+require('songbird')
 
 app.listen(8000);
 
-app.get('/', myfunct, function(req, res) {
-	console.log("Hey Node.js!")
-	res.send("check \n")
+
+app.get('/callback', function (req, res) {
+	//BAD
+	//Using callback
+	fs.readdir(__dirname, function (err, files) {
+		console.log(files);
+		res.send(files + '\n');
+	});
 });
 
-function myfunct(req, res, next) {
-	console.log("in next -> ");
+app.get('/promise', function (req, res) {
+	//BETTER
+    //Using promises
+	fs.promise.readdir(__dirname).then(function (files) {
+		console.log(files);
+	});
+});
+
+app.get('/async', ls, function (req, res) {
+	
+});
+
+app.get('/', myFunct, function (req, res) {
+	console.log("Hey, Node.js!")
+	res.end("Client - Hey, Node.js! \n")
+});
+
+function myFunct(req, res, next) {
+	console.log("in the 'myFunct' middleware at server");
+	res.write("in the 'myFunct' middleware in the client \n");
 	next();
 }
+
+async function ls(req, res, next) {
+	var files = await fs.promise.readdir(__dirname);
+	console.log(files);
+	next();
+}
+ls().catch(function (err) {
+	console.log(err.stack);
+});
+
+
+
+//let http = require('http')
+//
+//
+//
+//http.createServer((req, res) => {
+//
+//   
+//    fs.promise.readdir(__dirname).then(function (files) {
+//        console.log("using promise " + files)
+//    })
+//
+//
+//    fs.promise.readFile("control-flow.js").then(function (files) {
+//        console.log("using promise readfile " + files)
+//    })
+//
+//
+//    //BEST
+//    //Using async await
+//    async function ls() {
+//      let files = await fs.promise.readdir(__dirname);
+//      console.log(files);
+//    }
+//    ls().catch(function(err) {
+//      console.log(err.stack);
+//    });
+//
+//}).listen(8000, '127.0.0.1');
